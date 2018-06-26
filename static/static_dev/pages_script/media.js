8f2c7a7955fdde8f31c7ac87cb870c7a04473565
Electoratium <elecotatium@gmail.com>
2018-06-26 20:29:24 +0300

    add media queries for phones with landscape orientation

--- static/static_dev/pages_script/media.js
+++ static/static_dev/pages_script/media.js
@@ -3,6 +3,7 @@ window.addEventListener('resize', getAdaptive);
 
 function  getAdaptive() {
     const deviceWidth = document.documentElement.clientWidth,
+        deviceHeight = document.documentElement.clientHeight,
         minWidth = 1024;
     let carouselSlides = document.getElementsByClassName('carousel-inner')[0].children,
         authorImg = document.getElementsByClassName('author-img')[0].firstElementChild,
@@ -19,22 +20,20 @@ function  getAdaptive() {
 
         elem.setAttribute('src', newpath);
     }
-    if( deviceWidth >= minWidth) {
-        for(let slide of carouselSlides) {
-            changeImagePath(slide.children[0])
-        }
 
+    // ladscape  orientation smartphone
+    if ( deviceWidth > deviceHeight) {
+        if( deviceWidth >= minWidth) {
+            for(let slide of carouselSlides) {
+                changeImagePath(slide.children[0])
+            }
+
+        }
 
         changeImagePath(authorImg);
 
         for(let newItem of newArrivalsItems) {
             changeImagePath(newItem.firstElementChild);
         }
-
-
-
-
     }
-
-
 }
